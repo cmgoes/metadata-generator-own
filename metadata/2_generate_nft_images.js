35eb67b3051269a528d1822f3b4a6b731b0cdb16
prince-aloha <prince.gao@aloha.software>
2022-01-21 05:00:06 +0000

    modified image generator

--- metadata/2_generate_nft_images.js
+++ metadata/2_generate_nft_images.js
@@ -18,32 +18,47 @@ async function main() {
 const generateNftImg = function (metadata, filename) {
   if (metadata["attributes"].length > 1) {
     // load the first layer
-    let img = images(
-      TRAITS_IMAGES_PATH +
+    let path = TRAITS_IMAGES_PATH +
+      "/" +
+      metadata["attributes"][0]["value"] +
+      "/" +
+      metadata["attributes"][1]["trait_type"] +
+      "/" +
+      metadata["attributes"][1]["value"] +
+      ".png"
+
+    if (!fs.existsSync(path)) {
+      path = TRAITS_IMAGES_PATH +
         "/" +
         metadata["attributes"][0]["value"] +
         "/" +
         metadata["attributes"][1]["trait_type"] +
         "/" +
         metadata["attributes"][1]["value"] +
-        ".png"
-    );
+        ".PNG"
+    }
+    let img = images(path);
     // stack other layers in order
     for (let i = 2; i < metadata["attributes"].length; i++) {
-      img.draw(
-        images(
-          TRAITS_IMAGES_PATH +
-            "/" +
-            metadata["attributes"][0]["value"] +
-            "/" +
-            metadata["attributes"][i]["trait_type"] +
-            "/" +
-            metadata["attributes"][i]["value"] +
-            ".png"
-        ),
-        0,
-        0
-      );
+      let path = TRAITS_IMAGES_PATH +
+        "/" +
+        metadata["attributes"][0]["value"] +
+        "/" +
+        metadata["attributes"][i]["trait_type"] +
+        "/" +
+        metadata["attributes"][i]["value"] +
+        ".png"
+      if (!fs.existsSync(path)) {
+        path = TRAITS_IMAGES_PATH +
+          "/" +
+          metadata["attributes"][0]["value"] +
+          "/" +
+          metadata["attributes"][i]["trait_type"] +
+          "/" +
+          metadata["attributes"][i]["value"] +
+          ".PNG"
+      }
+      img.draw(images(path), 0, 0);
     }
     img.save(NFT_IMAGES_DIR + "/" + filename + ".png", { quality: 100 }); //Save the image to a file, with the quality of 100
   }
